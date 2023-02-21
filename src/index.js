742184af448b5961323145b5e93c567c2bff838f
ForestDr <nikita2001395@gmail.com>
2023-02-21 16:12:26 +0200

    add js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,105 @@
 import './css/styles.css';
+import './css/countries-list.css';
+import './css/country-info-card.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const fieldSearchEl = document.querySelector('input#search-box');
+const listCountriesEl = document.querySelector('.country-list');
+const cardCountryEl = document.querySelector('.country-info');
+
+listCountriesEl.classList.add('country-list-styles-none');
+
+fieldSearchEl.addEventListener(
+  'input',
+  debounce(searchCountries, DEBOUNCE_DELAY)
+);
+
+function searchCountries(e) {
+  const nameCountry = e.target.value.trim();
+
+  if (!nameCountry.length) {
+    removeMarkup(listCountriesEl, cardCountryEl);
+
+    return;
+  }
+
+  fetchCountries(nameCountry).then(data => {
+    if (data.length > 10) {
+      removeMarkup(listCountriesEl, cardCountryEl);
+
+      Notify.info('Too many matches found. Please enter a more specific name.');
+      return;
+    }
+
+    if (data.length >= 2) {
+      removeMarkup(cardCountryEl);
+      const markup = createListCountriesMarkup(data);
+      addMarkup(listCountriesEl, markup);
+
+      return;
+    }
+
+    removeMarkup(listCountriesEl);
+    const markup = createInfoCardCountryMarkup(data);
+    addMarkup(cardCountryEl, markup);
+  });
+}
+
+function createListCountriesMarkup(dataCountries) {
+  return dataCountries
+    .map(
+      dataCountry =>
+        `
+          <li class="country-list-item">
+            <div class="country-list-thumb">
+              <img class="country-list-img" src="${dataCountry.flags.svg}" alt="${dataCountry.name.official}" />
+            </div>
+            <p class="country-list-name">${dataCountry.name.official}</p>
+          </li>
+        `
+    )
+    .join('');
+}
+
+function createInfoCardCountryMarkup([dataCountry]) {
+  return `
+          <div class="country-card-box">
+            <div class="country-card-thumb">
+              <img class="country-card-img" src="${
+                dataCountry.flags.svg
+              }" alt="${dataCountry.name.official}" />
+            </div>
+            <h1 class="country-card-title">
+              ${dataCountry.name.official}
+            </h1>
+          </div>
+          <ul class="country-card-info-list">
+            <li class="country-card-info-list-item">
+              <b>Capital: </b>
+              ${dataCountry.capital}
+            </li>
+            <li class="country-card-info-list-item">
+              <b>Population: </b>
+              ${dataCountry.population}
+            </li>
+            <li class="country-card-info-list-item">
+              <b>Languages: </b>
+              ${{ ...dataCountry.languages }}
+            </li>
+          </ul>
+        `;
+}
+
+function addMarkup(refEl, markup) {
+  refEl.innerHTML = markup;
+}
+
+function removeMarkup(...refsEl) {
+  refsEl.forEach(refEl => {
+    refEl.innerHTML = '';
+  });
+}
